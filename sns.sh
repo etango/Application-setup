f94eef70f44eff93caab387397e05ded5d726b0c
Elton Tang <etang315@gmail.com>
2015-11-21 20:51:23 -0600

    Create sns.sh

---
+++ sns.sh
@@ -0,0 +1,9 @@
+ARN=' aws sns create-topic ...name et-mp2'
+
+echo "This is the ARN: $ARN"
+
+aws sns set topic attributes --topic arn $ARN --attribute name DisplayName --attribute value et-mp2
+
+aws sns subscribe --topic-arn $ARN --protocol sms --notification-endpoint $email
+
+aws sns publish --topic-arn arn:aws:sns:us-east-1:5032847113842:et-mp2 --message "Thank You for subscribing!"
